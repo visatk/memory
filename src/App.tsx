3ced32f7ef71967ad0428872fb1b4f0925fdb577
visatk <visatk29@gmail.com>
2026-04-15 12:45:07 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
+import { AuthProvider } from './context/AuthContext';
 import Landing from './pages/Landing';
 import TestCards from './pages/TestCards';
 import FakeAddress from './pages/FakeAddress';
 import Forum from './pages/Forum';
 import Thread from './pages/Thread';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
   return (
-    <Routes>
-      {/* The Layout component wraps all child routes */}
-      <Route path="/" element={<Layout />}>
-        {/* Child routes injected into Layout's <Outlet /> */}
-        <Route index element={<Landing />} />
-        <Route path="test-cards" element={<TestCards />} />
-        <Route path="fake-address" element={<FakeAddress />} />
-        <Route path="forum" element={<Forum />} />
-        <Route path="forum/:id" element={<Thread />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="test-cards" element={<TestCards />} />
+          <Route path="fake-address" element={<FakeAddress />} />
+          <Route path="forum" element={<Forum />} />
+          <Route path="forum/:id" element={<Thread />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
