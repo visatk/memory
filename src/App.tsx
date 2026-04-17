b6464a63f3fa6702869778284fe3bfabcb825d1f
visatk <visatk29@gmail.com>
2026-04-17 18:37:40 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { AuthProvider } from './context/AuthContext';
-import Landing from './pages/Landing';
 import TestCards from './pages/TestCards';
 import FakeAddress from './pages/FakeAddress';
 import Forum from './pages/Forum';
 import Thread from './pages/Thread';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Profile from './pages/Profile'; // <-- Imported new Profile page
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
+          {/* Forum is now the default Home index */}
+          <Route index element={<Forum />} />
           <Route path="test-cards" element={<TestCards />} />
           <Route path="fake-address" element={<FakeAddress />} />
-          <Route path="forum" element={<Forum />} />
           <Route path="forum/:id" element={<Thread />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          {/* Registered the Profile Route */}
           <Route path="profile/:username" element={<Profile />} />
         </Route>
       </Routes>
