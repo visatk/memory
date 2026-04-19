e912df5e76d8568d69a711ad7915b44653846f01
visatk <visatk29@gmail.com>
2026-04-20 01:02:38 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { AuthProvider } from './context/AuthContext';
+import { ToastProvider } from './context/ToastContext';
 
 // Pages
 import Forum from './pages/Forum';
@@ -18,26 +19,28 @@ import BinChecker from './pages/BinChecker';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Main Forum Routes */}
-          <Route index element={<Forum />} />
-          <Route path="forum/:id" element={<Thread />} />
-          
-          {/* Utility / Tool Routes */}
-          <Route path="test-cards" element={<TestCards />} />
-          <Route path="fake-address" element={<FakeAddress />} />
-          <Route path="card-checker" element={<CardChecker />} />
-          <Route path="bin-checker" element={<BinChecker />} />
-          
-          {/* Authentication & User Management Routes */}
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="verify-email" element={<VerifyEmail />} />
-          <Route path="profile/:username" element={<Profile />} />
-        </Route>
-      </Routes>
-    </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Main Forum Routes */}
+            <Route index element={<Forum />} />
+            <Route path="forum/:id" element={<Thread />} />
+            
+            {/* Utility / Tool Routes */}
+            <Route path="test-cards" element={<TestCards />} />
+            <Route path="fake-address" element={<FakeAddress />} />
+            <Route path="card-checker" element={<CardChecker />} />
+            <Route path="bin-checker" element={<BinChecker />} />
+            
+            {/* Authentication & User Management Routes */}
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="verify-email" element={<VerifyEmail />} />
+            <Route path="profile/:username" element={<Profile />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </ToastProvider>
   );
 }
