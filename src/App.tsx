5114f3148f8f03eae2675c7b49734d22290e4951
visatk <visatk29@gmail.com>
2026-04-19 23:57:47 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { AuthProvider } from './context/AuthContext';
-import TestCards from './pages/TestCards';
-import FakeAddress from './pages/FakeAddress';
-import CardChecker from './pages/CardChecker';
-import BinChecker from './pages/BinChecker'; // <-- Import component
+
+// Pages
 import Forum from './pages/Forum';
 import Thread from './pages/Thread';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import VerifyEmail from './pages/VerifyEmail';
 import Profile from './pages/Profile';
+
+// Tools
+import TestCards from './pages/TestCards';
+import FakeAddress from './pages/FakeAddress';
+import CardChecker from './pages/CardChecker';
+import BinChecker from './pages/BinChecker';
 
 export default function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
+          {/* Main Forum Routes */}
           <Route index element={<Forum />} />
+          <Route path="forum/:id" element={<Thread />} />
+          
+          {/* Utility / Tool Routes */}
           <Route path="test-cards" element={<TestCards />} />
           <Route path="fake-address" element={<FakeAddress />} />
           <Route path="card-checker" element={<CardChecker />} />
-          <Route path="bin-checker" element={<BinChecker />} /> {/* <-- Add Route */}
-          <Route path="forum/:id" element={<Thread />} />
+          <Route path="bin-checker" element={<BinChecker />} />
+          
+          {/* Authentication & User Management Routes */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="verify-email" element={<VerifyEmail />} />
           <Route path="profile/:username" element={<Profile />} />
         </Route>
       </Routes>
